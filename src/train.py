7dfaec49a433b9ad10df44e76c4c24250e90a988
SerLap-ctrl <s.laptev2017@yandex.ru>
2023-11-07 20:27:42 +0700

    Logistic Regression an pipeline

--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from skimage.io import imread_collection
 from skimage.transform import resize
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
 
 def load_images(data_frame, column_name):
@@ -37,8 +37,8 @@ def load_data(data_path):
 def main(repo_path):
     train_csv_path = repo_path / "data/prepared/train.csv"
     train_data, labels = load_data(train_csv_path)
-    rf = RandomForestClassifier()                  # <- 
-    trained_model = rf.fit(train_data, labels)     # <- 
+    rl = LogisticRegression()                  # <- 
+    trained_model = rl.fit(train_data, labels)     # <- 
     dump(trained_model, repo_path / "model/model.joblib")
 
 
